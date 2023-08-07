daceaaee29d6805e9ed2cc96152a8025561a74d7
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-08 00:41:05 +0200

    name and placeholder

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import './Meme.css';
+import React, { useState, useEffect } from "react";
+import "./Meme.css";
 
 export default function MemeGenerator() {
+  const [topText, setToptext] = useState("");
+  const [bottomText, setBottomtext] = useState("");
+  const [randomImg, setRandomImg] = useState("");
+  const [allMemeImg, setAllMemeImg] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((response) => {
+        const { memes } = response.data;
+        console.log(memes);
+        setAllMemeImg(memes);
+      });
+  });
+
   return (
-    <div>MemeGenerator</div>
-  )
+    <div className="meme-from">
+      <div className="inputlar">
+        <input
+          type="text"
+          className="input"
+          name="topText"
+          placeholder="enter top text"
+        />
+      </div>
+    </div>
+  );
 }
